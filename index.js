9e0636ad9bf61c34b0a044284e4105f65249572d
KelvJoker <kelvinkv2030@gmail.com>
2025-11-14 00:04:53 -0300

    adjust

---
+++ index.js
@@ -0,0 +1,5 @@
+// Este arquivo existe apenas para satisfazer a tentativa do Render de executar 'node index.js'
+// O verdadeiro servidor é iniciado pelo script npm start
+
+// Importa o script server.js que cuida de iniciar o backend
+require('./server.js');
